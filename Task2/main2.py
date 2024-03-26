c426a79fe1032e453829edbf78636765cb6a68d3
DaniilKonsevych <160627859+DaniilKonsevych@users.noreply.github.com>
2024-03-26 16:38:57 +0100

    Update

--- Task2/main2.py
+++ Task2/main2.py
@@ -1,18 +1,14 @@
 def get_cats_info(path):
     data_list =  []
     with open(path, "r+") as cat_info:
+        for line in cat_info:
+            dictionary = {"id" : "" , "name" : "" , "age" : ""}
 
-        cat_info.seek(0)
-        cat_info.write("\n")
+            line = line.replace("\n", "").split(",")
 
-        for line in (cat_info):
-            dictionary = {"id:" : "" , "name" : "" , "age" : ""}
-
-            line = line.split(",")
-
-            dictionary.update({"id:" : line[0]})
-            dictionary.update({"name:" : line[1]})
-            dictionary.update({"age:" : line[2]})
+            dictionary.update({"id" : line[0]})
+            dictionary.update({"name" : line[1]})
+            dictionary.update({"age" : line[2]})
 
             data_list.append(dictionary)
     return data_list
